a1d8c18eda3cb309ee39a7c73de4b7f7b5c2f63c
RafaelMunarin <rafael.munarin@unidavi.edu.br>
2024-12-04 22:27:37 -0300

    Update apiBaseUrl.js

--- VoluntarioApp/src/config/apiBaseUrl.js
+++ VoluntarioApp/src/config/apiBaseUrl.js
@@ -1,5 +1,3 @@
-// const API_BASE_URL = 'http://192.168.1.9:5000/api'; 
-const API_BASE_URL =  `http://10.7.3.78:5000/api/`
-// const API_BASE_URL =  `http://10.7.3.117:5000/api/`
+const API_BASE_URL =  `http://192.168.1.4:5000/api/`
 
 export default API_BASE_URL
